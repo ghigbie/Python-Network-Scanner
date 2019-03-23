8b0007c6aeffd11e4aff9fbd4c440602b1e23adc
ghigbie <gehigbie@gmail.com>
2019-03-23 08:48:56 -0600

    Set broadcast mac address

--- scratch_network_scanner.py
+++ scratch_network_scanner.py
@@ -7,6 +7,7 @@ import scapy.all as scapy
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     print(arp_request.summary())
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
 
 
 scan("10.0.2.1/24")
