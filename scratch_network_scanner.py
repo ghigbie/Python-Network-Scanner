5a952d2c7d84c6cfed8f25094fb63e705c3f1067
ghigbie <gehigbie@gmail.com>
2019-03-23 08:53:54 -0600

    Edit braodcast summary

--- scratch_network_scanner.py
+++ scratch_network_scanner.py
@@ -8,6 +8,7 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     print(arp_request.summary())
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    print(broadcast.summary())
 
 
 scan("10.0.2.1/24")
