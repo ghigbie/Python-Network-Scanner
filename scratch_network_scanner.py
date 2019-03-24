5f4ac169525431e707cf4f92af320bfd38a1a308
ghigbie <gehigbie@gmail.com>
2019-03-24 08:30:21 -0600

    Add show methods

--- scratch_network_scanner.py
+++ scratch_network_scanner.py
@@ -6,9 +6,14 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
+    arp_request.show()
+    print('########################')
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    broadcast.show()
+    print('########################')
     arp_request_broadcast = broadcast/arp_request
-    print(broadcast.summary())
+    arp_request_broadcast.show()
+    print('########################')
 
 
 scan("10.0.2.1/24")
